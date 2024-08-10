4e550ce8c045dbe31b7fb2e470e8f3cc5ac98487
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-10 12:18:51 +0100

    tidy up

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,3 @@
 export default async function DashboardPage() {
-  const request = await fetch('https://flaggy-ten.vercel.app/api/getFlags');
-  const flags = await request.json();
-
-  console.log(flags);
   return <div>DASHBOARD</div>;
 }
