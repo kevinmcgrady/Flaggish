9d90ab91ed63a20c52088cfa5d208e1c760fc80e
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-10 09:53:17 +0100

    @test/api route

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
+
 export default function DashboardPage() {
+  useEffect(() => {
+    fetch('https://flaggy-ten.vercel.app/api/getFlags')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.log('ERROR', error));
+  }, []);
   return <div>DASHBOARD</div>;
 }
