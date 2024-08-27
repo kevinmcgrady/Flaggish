5790c458b03a6a688b07b4edeed4595d107132e7
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-27 14:53:31 +0100

    refactor apiKeyInput to use new hook

--- src/components/dashboard/ApiKeyInput.tsx
+++ src/components/dashboard/ApiKeyInput.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { Copy, Eye, EyeOff, Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import { Copy, Eye, EyeOff } from 'lucide-react';
 import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { useToast } from '@/components/ui/use-toast';
 import { useCopyToClipboard } from '@/hooks/useCopyToClipboard';
+import { useSubmitForm } from '@/hooks/useSubmitForm';
 import { updateKey } from '@/queries/keys/updateKey';
 import { ApiKeyType } from '@/types/ApiKeyType';
+
+import { SubmitButton } from '../site/SubmitButton';
 
 type ApiKeyInoutProps = {
   label: string;
@@ -26,28 +27,19 @@ export const ApiKeyInput = ({
   projectId,
 }: ApiKeyInoutProps) => {
   const [isHidden, setIsHidden] = useState<boolean>(true);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const { toast } = useToast();
+  const { isLoading, onSubmit } = useSubmitForm();
   const { copyToClipboard } = useCopyToClipboard();
-  const router = useRouter();
 
   const handleGenerateApiKey = async () => {
-    try {
-      setIsLoading(true);
-      await updateKey(projectId, type);
-      router.refresh();
-      toast({
+    await onSubmit({
+      successToast: {
         title: 'Key updated',
         description: 'Your key has been updated.',
-      });
-    } catch (error) {
-      toast({
-        title: 'Oops!',
-        description: 'There was a problem, please try again.',
-      });
-    } finally {
-      setIsLoading(false);
-    }
+      },
+      callback: async () => {
+        await updateKey(projectId, type);
+      },
+    });
   };
 
   const handleCopyToClipboard = () => {
@@ -63,16 +55,16 @@ export const ApiKeyInput = ({
   };
 
   return (
-    <section className='bg-white p-4 rounded-xl mt-4'>
+    <section className='bg-white p-4 rounded-xl mt-4 space-y-4'>
       <Label>{label}</Label>
       <div className='flex gap-4 items-center'>
         <Input
-          className='text-muted-foreground text-sm mt-2'
+          className='text-muted-foreground text-sm'
           readOnly
           value={apiKey}
           type={isHidden ? 'password' : 'text'}
         />
-        <div className='flex gap-2 mt-2'>
+        <div className='flex gap-2'>
           <Button onClick={handleReveal} variant='outline' size='icon'>
             {isHidden ? <Eye size={15} /> : <EyeOff size={15} />}
           </Button>
@@ -81,14 +73,9 @@ export const ApiKeyInput = ({
           </Button>
         </div>
       </div>
-
-      <Button onClick={handleGenerateApiKey} className='mt-4'>
-        {isLoading ? (
-          <Loader2 size={15} className='animate-spin' />
-        ) : (
-          'Generate new key'
-        )}
-      </Button>
+      <SubmitButton isLoading={isLoading} onClick={handleGenerateApiKey}>
+        Generate new key
+      </SubmitButton>
     </section>
   );
 };
