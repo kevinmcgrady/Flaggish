a5b199cea7b0b6f2d0f59d5c24f33f6c658842ac
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-28 08:38:37 +0100

    @test/getProject

---
+++ src/queries/projects/getProject.spec.ts
@@ -0,0 +1,64 @@
+import { currentUser, User } from '@clerk/nextjs/server';
+import { Project } from '@prisma/client';
+import { beforeEach, describe, expect,it, vi } from 'vitest';
+
+import { db } from '@/lib/db';
+
+import { getProject } from './getProject';
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+vi.mock('@clerk/nextjs/server');
+vi.mock('@/lib/db', () => {
+  return {
+    db: {
+      project: {
+        findFirst: vi.fn(),
+      },
+    },
+  };
+});
+
+const user = { emailAddresses: [{ emailAddress: 'email' }], id: 'user-id' };
+const project: Project = {
+  id: 'project-id',
+  name: 'project-name',
+  description: 'project-description',
+  clientApiKey: 'client-key',
+  secretApiKey: 'secret-key',
+  isActive: true,
+  slug: 'project-slug',
+  userId: 'user-id',
+};
+
+describe('getProject', () => {
+  it('should return null if the user is not logged in', async () => {
+    const authMock = vi.mocked(currentUser).mockResolvedValue(null);
+    const dbMock = vi.mocked(db.project.findFirst);
+    const actual = await getProject('project-sluf');
+
+    expect(authMock).toHaveBeenCalledTimes(1);
+    expect(dbMock).toHaveBeenCalledTimes(0);
+    expect(actual).toBe(null);
+  });
+
+  it('should return the users project', async () => {
+    const authMock = vi.mocked(currentUser).mockResolvedValue(user as User);
+    const dbMock = vi.mocked(db.project.findFirst).mockResolvedValue(project);
+    const actual = await getProject('project-slug');
+
+    expect(authMock).toHaveBeenCalledTimes(1);
+    expect(dbMock).toHaveBeenCalledTimes(1);
+
+    expect(dbMock).toHaveBeenCalledWith({
+      where: {
+        userId: 'user-id',
+        slug: 'project-slug',
+      },
+    });
+
+    expect(actual).toBe(project);
+  });
+});
