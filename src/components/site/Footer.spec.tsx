a6d061c20e35a01b7cddc718766e47f294c3575b
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-30 20:55:56 +0100

    @test/footer

---
+++ src/components/site/Footer.spec.tsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+
+import { render,screen } from '@testing-library/react';
+import { describe, expect,it } from 'vitest';
+
+import { Footer } from '@/components/site/Footer';
+import { navItems } from '@/config/navItems';
+
+describe('Footer', () => {
+  it('should display the nav links', () => {
+    render(<Footer />);
+
+    navItems.forEach((item) => {
+      const link = screen.getByText(item.text);
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', item.url);
+    });
+  });
+});
