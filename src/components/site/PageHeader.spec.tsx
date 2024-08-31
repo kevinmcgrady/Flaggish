5cb8767d4282fc1f0a3defe2fc214264f27de333
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-08-31 08:56:46 +0100

    @test/PageHeader

---
+++ src/components/site/PageHeader.spec.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { PageHeader } from './PageHeader';
+
+const title = 'title';
+const description = 'description';
+const ctaComponent = <p>cta-component</p>;
+
+describe('<PageHeader />', () => {
+  it('should display the correct details', () => {
+    render(
+      <PageHeader
+        title={title}
+        description={description}
+        ctaComponent={ctaComponent}
+      />,
+    );
+
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(description)).toBeInTheDocument();
+    expect(screen.getByText('cta-component')).toBeInTheDocument();
+  });
+
+  it('should hide the cta', () => {
+    render(<PageHeader title={title} description={description} />);
+
+    expect(screen.queryByText('cta-component')).not.toBeInTheDocument();
+  });
+});
